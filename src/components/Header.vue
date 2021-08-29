f77290bf5d45a9c96e7aad24c34db25315ab3e48
Shraban-7 <shakuatshraban@gmail.com>
2021-08-29 17:48:41 +0600

    ok

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  components: { Button },
+  name: "Header",
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
+
